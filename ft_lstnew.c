45fccf03e98eec02b2dd3eb0bcb8ee267254fbdc
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-04-19 15:13:26 +0100

    Add files via upload

---
+++ ft_lstnew.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+#include <stdio.h>
+
+/*Allocates (with malloc(3)) and returns a new node.
+The member variable ’content’ is initialized with
+the value of the parameter ’content’. The variable
+’next’ is initialized to NULL. Return value: The new node*/
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
